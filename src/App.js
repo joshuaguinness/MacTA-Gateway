8aa7a7295ff9386fd43c93dbaa2f238c1719926d
Joshua Guinness <joshua.guinness@gmail.com>
2020-10-29 14:54:29 -0400

    Edited text for pull request test purposes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - It will be fun!
         </a>
       </header>
     </div>
