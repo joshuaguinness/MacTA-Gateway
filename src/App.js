54a0fc005307e113c5030f4977d1fa65d7f5e8ad
Arkin Modi <arkinmodi@gmail.com>
2020-11-11 18:20:06 -0500

    Added temp nav button to apply position

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import { HashRouter as Router, Route, NavLink } from "react-router-dom";
 import CreatePosting from "./components/CreatePosting.js";
 import ViewPostings from "./components/ViewPostings.js";
+import ApplyPosition from "./components/ApplyPosition.js";
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
 class App extends React.Component {
@@ -17,12 +18,18 @@ class App extends React.Component {
           <NavLink exact to="/viewpostings">
             View Postings
           </NavLink>
+          <NavLink exact to="/applyposition">
+            Apply for Position
+          </NavLink>
 
           <Route exact path="/createposting">
             <CreatePosting />
           </Route>
           <Route exact path="/viewpostings">
             <ViewPostings />
+          </Route>
+          <Route exact path="/applyposition">
+            <ApplyPosition />
           </Route>
         </Router>
       </div>
