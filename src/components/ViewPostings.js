19b91f71dc850a5ce7ea2cd0a32a20dc2e5315c6
unknown <marin@mcmaster.ca>
2020-11-14 01:18:36 -0500

    basic outline

--- src/components/ViewPostings.js
+++ src/components/ViewPostings.js
@@ -1,12 +1,27 @@
 import React from "react";
+import './ViewPostings.css';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 
 class ViewPostings extends React.Component {
   render() {
     return (
-      <div>
-        <p>View Postings Page</p>
-        <p> placeholder </p>
-      </div>
+      <Box component="div" className="background">
+      	<Box component="div" className="foreground">
+      		<Grid container spacing={0}>
+        		<Grid item xs={12}>
+        			<Paper class="pOutline filters" variant="outlined" square>Filters</Paper>
+        		</Grid>
+        		<Grid item xs={4}>
+         			<Paper class="pOutline posts" variant="outlined" square>Posts</Paper>
+        		</Grid>
+        		<Grid item xs={8}>
+        			<Paper class="pOutline details" variant="outlined" square>Details</Paper>
+        		</Grid>
+      		</Grid>
+      	</Box>
+      </Box>
     );
   }
 }
