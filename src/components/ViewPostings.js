c912353aa6b07c2de90fe970beaa7b0000f18ce2
unknown <marin@mcmaster.ca>
2020-11-14 11:36:01 -0500

    post details appear when thumbnail is clicked

--- src/components/ViewPostings.js
+++ src/components/ViewPostings.js
@@ -1,27 +1,47 @@
 import React from "react";
 import './ViewPostings.css';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
+import {Grid, Box, Button} from '@material-ui/core';
 
 class Thumbnail extends React.Component {
+	
+	constructor(props) {
+		super(props);
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+	handleChange(e) {
+		console.log(e)
+		this.props.displayDetails(e.currentTarget.dataset.index)
+	}
+
 	render() {
 		return(
 			<div>
-         		{this.props.posts.map(
-         		({id, title, dept}) =>
-         			<div key={id}>
-         				<p> {title} </p>
-         			</div>
-         		)}
+         		 {this.props.posts.map(
+         		 ({id, title, dept, desc, hours, resp}) =>
+         			<Button key={id} data-index={id} onClick={this.handleChange}>
+         		 		<p> {title} </p>
+         				<p> {dept} </p>
+         		 	</Button>
+         		 )}
          	</div>
 		);
 	}
 }
 
 class PostDetails extends React.Component {
-	rednder() {
+	render() {
 		return(
 			<div>
+			{this.props.cDetails.map(
+         		({id, title, dept, desc, hours, resp}) =>
+         			<div key={id}>
+         				<p> {title} </p>
+         				<p> {desc} </p>
+         				<p> {hours} </p>
+         				<p> {resp} </p>
+         			</div>
+         		)}
 			</div>
 		);
 	}
@@ -31,15 +51,38 @@ class ViewPostings extends React.Component {
 
   constructor(props) {
   	super(props);
-  	this.state = {posts: [{id: 0,
+  	this.changeCurrentDetails = this.changeCurrentDetails.bind(this)
+  	this.state = {posts: [
+  			{id: 0,
   			 title: "ENGINEER 1P13 EPIC Lab IAI",
-  			 dept: "Faculty of Engineering"},
+  			 dept: "Faculty of Engineering",
+  			 hours: "Full Time",
+  			 resp: "Code",
+  			 desc: "placeholder"},
 
   			 {id: 1,
   			 title: "ENGINEER 1P13 EPIC Lab IAI",
-  			 dept: "Faculty of Engineering"}
+  			 dept: "Faculty of Engineering",
+  			 hours: "Part Time",
+  			 resp: "CAD",
+  			 desc: "placeholder"}
+  			],
 
-  			]};
+  				currDetails: []};
+
+
+  	let postsHolder = this.state.posts;
+  	let detailsHolder = this.state.currDetails;
+  	detailsHolder[0] = postsHolder[0];
+  	this.setState({detailsHolder});
+  }
+
+  changeCurrentDetails(id) {
+  	console.log(id)
+  	let postsHolder = this.state.posts;
+  	let detailsHolder = this.state.currDetails;
+  	detailsHolder[0] = postsHolder[id];
+  	this.setState({detailsHolder});
   }
 
   render() {
@@ -52,11 +95,14 @@ class ViewPostings extends React.Component {
         		</Grid>
         		<Grid item xs={4}>
          			<Box component="div" className="pOutline posts">Posts
-         				<Thumbnail posts={this.state.posts} />
+         				<Thumbnail posts={this.state.posts} displayDetails={this.changeCurrentDetails} />
          			</Box>
         		</Grid>
         		<Grid item xs={8}>
-        			<Box component="div" className="pOutline details">Details</Box>
+        			<Box component="div" className="pOutline details">Details
+        				<PostDetails cDetails={this.state.currDetails} />
+        				test
+        			</Box>
         		</Grid>
       		</Grid>
       	</Box>
