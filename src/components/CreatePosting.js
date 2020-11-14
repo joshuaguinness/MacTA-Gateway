6ec9947bba24088a9ab7053f1a953eb495a68ccd
Joshua Guinness <joshua.guinness@gmail.com>
2020-11-14 13:54:27 -0500

    Start of setup of skeleton for create ta posting functionality

--- src/components/CreatePosting.js
+++ src/components/CreatePosting.js
@@ -1,13 +1,79 @@
 import React from "react";
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import TabPanel from '@material-ui/lab/TabPanel';
 
 class CreatePosting extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      courseTitle: "",
+      courseCode: "",
+      courseDescription: "",
+      courseSkills: "",
+      courseResponsibilities: "",
+      deadline: "",
+      startTime: "",
+      requiredDocuments: "",
+      additionalApplicationQuestions: [],
+    };
+  }
+
   render() {
     return (
       <div>
-        <p>Create Posting Page</p>
+        {/* <AppBar position="static">
+          <Tabs
+            // value={value}
+            // onChange={handleChange}
+            aria-label="simple tabs example"
+          >
+            <Tab label="Item One" />
+            <Tab label="Item Two"  />
+            <Tab label="Item Three"  />
+          </Tabs>
+        </AppBar>
+        <TabPanel  index={0}>
+          Item One
+        </TabPanel>
+        <TabPanel  index={1}>
+          Item Two
+        </TabPanel>
+        <TabPanel  index={2}>
+          Item Three
+        </TabPanel> */}
       </div>
     );
   }
 }
 
 export default CreatePosting;
+
+/* Add post details
+- Add course title
+- Add course code
+- Add description
+- Add required skills
+- Add job responsibilities 
+
+Set deadline 
+- Select month, day, year 
+- Select hours, minutes 
+
+Set start time 
+- Select month, day, year 
+- Select hours, minutes 
+
+Set required documents 
+Add additional application questions 
+*/
+
+/*
+Creating Posting Component
+Sub component under each one
+State store in parent component
+
+
+*/
