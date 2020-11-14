4cb73dac00b68af1eab1ebc8f64712548d067732
unknown <marin@mcmaster.ca>
2020-11-14 03:10:29 -0500

    post thumbnails function

--- src/components/ViewPostings.js
+++ src/components/ViewPostings.js
@@ -1,23 +1,62 @@
 import React from "react";
 import './ViewPostings.css';
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 
+class Thumbnail extends React.Component {
+	render() {
+		return(
+			<div>
+         		{this.props.posts.map(
+         		({id, title, dept}) =>
+         			<div key={id}>
+         				<p> {title} </p>
+         			</div>
+         		)}
+         	</div>
+		);
+	}
+}
+
+class PostDetails extends React.Component {
+	rednder() {
+		return(
+			<div>
+			</div>
+		);
+	}
+}
+
 class ViewPostings extends React.Component {
+
+  constructor(props) {
+  	super(props);
+  	this.state = {posts: [{id: 0,
+  			 title: "ENGINEER 1P13 EPIC Lab IAI",
+  			 dept: "Faculty of Engineering"},
+
+  			 {id: 1,
+  			 title: "ENGINEER 1P13 EPIC Lab IAI",
+  			 dept: "Faculty of Engineering"}
+
+  			]};
+  }
+
   render() {
     return (
       <Box component="div" className="background">
       	<Box component="div" className="foreground">
       		<Grid container spacing={0}>
         		<Grid item xs={12}>
-        			<Paper class="pOutline filters" variant="outlined" square>Filters</Paper>
+        			<Box component="div" className="pOutline filters">Filters</Box>
         		</Grid>
         		<Grid item xs={4}>
-         			<Paper class="pOutline posts" variant="outlined" square>Posts</Paper>
+         			<Box component="div" className="pOutline posts">Posts
+         				<Thumbnail posts={this.state.posts} />
+         			</Box>
         		</Grid>
         		<Grid item xs={8}>
-        			<Paper class="pOutline details" variant="outlined" square>Details</Paper>
+        			<Box component="div" className="pOutline details">Details</Box>
         		</Grid>
       		</Grid>
       	</Box>
