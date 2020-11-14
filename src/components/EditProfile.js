3bf28e538ff535fdcb94f9d925f72b63be5f8de0
Joshua Guinness <joshua.guinness@gmail.com>
2020-11-14 13:33:07 -0500

    More work done on profile aspect. Add functionality, styling, and missing content

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -1,20 +1,117 @@
 import React from "react";
+import PropTypes from 'prop-types'
 import TextField from "@material-ui/core/TextField";
+import { withStyles } from '@material-ui/core/styles';
+import "./Profile.css"
+
+const styles = {
+  root: {
+    marginLeft: '30px',
+    marginright: "30px",
+    marginTop: "10px",
+    marginBottom: "10px",
+    alignItems: "right"
+  },
+};
 
 class EditProfile extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleProgramChange = this.handleProgramChange.bind(this);
+  }
+
+  handlePhoneChange(e) {
+    this.props.onPhoneChange(e.target.value);
+  }
+
+  handleProgramChange(e) {
+    this.props.onProgramChange(e.target.value);
+  }
+
   render() {
+
+    const { classes } = this.props;
+
     return (
       <div>
         <h3>McMaster University</h3>
-        <TextField fullWidth id="programdetails" label="Program Details" defaultValue="Software Engineering and Society"/>
-        <TextField id="gpa" label="GPA" defaultValue={10.2}/>
-        <h5>Update Contact Details</h5>
-        <TextField fullWidth id="phonenumber" label="Phone Number" defaultValue="519-000-1998"/>
-        <TextField fullWidth id="backupemail" label="Backup Email" defaultValue="ilovesoftware@hotmail.com"/>
-        <TextField fullWidth id="mailingaddress" label="Mailing Address" defaultValue="1998 Main St. W, Hamilton, ON"/>
+        <TextField
+        className={classes.root}
+          fullWidth
+          id="programdetails"
+          variant="outlined"
+          label="Program Details"
+          defaultValue={this.props.program}
+          onChange={this.handleProgramChange}
+        />
+        <TextField
+        className={classes.root}
+          variant="outlined"
+          type="number"
+          className="gpa"
+          label="GPA"
+          defaultValue={this.props.gpa}
+        />
+        <h5>Contact Details</h5>
+        <TextField
+        className={classes.root}
+          id="phonenumber"
+          variant="outlined"
+          label="Phone Number"
+          defaultValue={this.props.phone}
+          onChange={this.handlePhoneChange}
+        />
+        <TextField
+        className={classes.root}
+          id="backupemail"
+          variant="outlined"
+          label="Backup Email"
+          defaultValue={this.props.backupemail}
+        />
+        <TextField
+        className={classes.root}
+          fullWidth
+          id="mailingaddress"
+          variant="outlined"
+          label="Mailing Address"
+          defaultValue={this.props.mailing}
+        />
+        <h5>Banking Details</h5>
+        <TextField
+        className={classes.root}
+          id="institution"
+          variant="outlined"
+          type="number"
+          label="Institution"
+          defaultValue={this.props.institution}
+        />
+        <TextField
+        className={classes.root}
+          id="transit"
+          variant="outlined"
+          type="number"
+          label="Transit"
+          defaultValue={this.props.transit}
+        />
+        <TextField
+        className={classes.root}
+          id="account"
+          variant="outlined"
+          type="number"
+          label="Account"
+          defaultValue={this.props.account}
+        />
+        <h5>Change Password</h5>
       </div>
     );
   }
 }
 
-export default EditProfile;
+EditProfile.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(EditProfile);
