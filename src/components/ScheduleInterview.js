4615ce5ff075f898a4457f054cf09095429dbac4
Arkin Modi <arkinmodi@gmail.com>
2020-11-14 19:01:58 -0500

    Initial creation of ScheduleInterview

---
+++ src/components/ScheduleInterview.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ScheduleInterview extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Schedule Interview Page</h1>
+      </div>
+    );
+  }
+}
+
+export default ScheduleInterview;
