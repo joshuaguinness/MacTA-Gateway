6a54a88d22081c496b57003ed7d09df3423f8d08
Arkin Modi <arkinmodi@gmail.com>
2020-11-14 20:28:27 -0500

    Added calendar and side bar

--- src/components/ScheduleInterview.js
+++ src/components/ScheduleInterview.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Calendar from "react-material-ui-calendar";
+import "../style/ScheduleInterview.css";
 
 class ScheduleInterview extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Schedule Interview Page</h1>
+      <div className={"background"}>
+        <div className={"foreground"}>
+          <h1>Schedule Interview Page</h1>
+          <div className={"body"}>
+          <div className={"calendar"}>
+            <Calendar
+              light
+              selection={(value) => console.log(value)}
+              generalStyle={{ height: "100%" }}
+            />
+          </div>
+          <div className={"scheduler"}>
+            <h4>November 2020</h4>
+            <h3>15</h3>
+            <h4>Sunday</h4>
+          </div>
+          </div>
+        </div>
       </div>
     );
   }
